d9d79c10dc31970b746b9a2644f859a64e7ab83f
SnB <segey.nebo@gmail.com>
2019-08-13 20:43:46 +0300

    Show carraing in js

--- src/js/bundle.js
+++ src/js/bundle.js
@@ -1,5 +1,32 @@
-function helloWorldFromBundle(a, b) {
-  console.log('Hello world from bundle', a, b);
+function sum(a, b) {
+  return a + b;
 }
 
-helloWorldFromBundle(1, 2);
+console.log('Simple sum', sum(1, 1));
+
+function sumCarrable(a) {
+  return b => a + b;
+}
+
+console.log('Carrable sum', sumCarrable(2)(1));
+
+const estimate = sumCarrable(2);
+
+console.log('Carrable sum estimate 1', estimate(1));
+console.log('Carrable sum estimate 2', estimate(2));
+
+
+function tooManyArgs(a, b, c, d) {
+  return a + b + c + d;
+}
+
+function lessArgs(obj) {
+  return obj.a + obj.b + obj.c + obj.d;
+}
+
+console.log(tooManyArgs(1, 2, 3, 4));
+console.log(lessArgs({
+  a: 1, b: 2, c: 3, d: 4,
+}));
+
+console.log('Finish');
