5db3bcfb0b0e0cf20ce1eedf2c8d00ba918fcc0d
SnB <segey.nebo@gmail.com>
2019-08-16 21:32:51 +0300

    Add js selector object

--- src/js.console/app.js
+++ src/js.console/app.js
@@ -1,3 +1,39 @@
 const alert = require('alert-node');
+const prompt = require('prompt-sync')();
 
-alert('howdy');
+const DEFAULT = '-ggggg';
+
+const QUESTION = {
+  1: 'How old are you?',
+  2: 'What is your name?',
+  [DEFAULT]: 'Go out!',
+};
+
+const ANSWER = {
+  1: 'Marry',
+  2: '5 years old',
+  3: '10 years old',
+  [DEFAULT]: "I don't get it",
+};
+
+function getValueFromStore(index, store) {
+
+  if (store[index]) {
+    return store[index];
+  }
+
+  return store[DEFAULT];
+}
+
+for (; ;) {
+
+  const questionIndex = parseInt(prompt('Select next question (0 - exit) '));
+  const answerIndex = parseInt(prompt('Select next answer (0 - exit) '));
+
+  console.log(getValueFromStore(questionIndex, QUESTION));
+  console.log(getValueFromStore(answerIndex, ANSWER));
+
+  if (questionIndex === 0 || answerIndex === 0) {
+    break;
+  }
+}
